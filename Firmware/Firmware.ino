4daab6cb1194cdf006a86982b29fb7fba0331b77
eduardocanova23 <64231485+eduardocanova23@users.noreply.github.com>
2023-10-24 18:36:12 +0200

    add gyroscope curve check

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -16,24 +16,30 @@
 #include <MeAuriga.h>
 #include "MeRGBLineFollower.h"
 #include "MeUltrasonicSensor.h"
-
+//#include "MeMegaPi.h"
 
 MeUltrasonicSensor ultraSensor(PORT_8);
 MeCompass MeCompass(PORT_6, ADDRESS3);
 MeRGBLineFollower LineFollower(PORT_7, ADDRESS1);
-
-
+MeGyro gyro;
+int state = 0;
+float ang;
 #define DT 50  //sampling period in milliseconds
-
+float offset;
 #define speed_limit 280
 int getDelaySensorWheel(float tension);
 int delayt = getDelaySensorWheel(12*190/255);
-
+float kp;
+int ref = 120;
+bool flagCurveLock = true;
+float lastLock = millis();
 void setup() {
   
-  
+
+  gyro.begin();
   LineFollower.begin();
-  LineFollower.setKp(0.05);
+  kp = 0.05;
+  LineFollower.setKp(kp);
 
   // initialization of the serial communication.
   Serial.begin(9600);
@@ -44,22 +50,189 @@ void setup() {
 
 void loop() {
   // Main loop
+  if (millis()-lastLock > 600){
+    flagCurveLock = true;
+  }
   LineFollower.loop();
-
   int offset = LineFollower.getPositionOffset();
+  gyro.update();
+  
   static int ref = 190;  // reference signal
-
+  ang = gyro.getAngleZ();
   int u = ref;  // control signal
-  
-  
-  waitNextPeriod();
 
- 
+  //waitNextPeriod();
+
   Serial.println(offset);
+  Serial.println(state);
+  Serial.println(ang);
 
   // get the new reference from the serial port is any.
   if (Serial.available() > 1) {  // something to read ?
     ref = Serial.parseInt();     // parse the value (ascii -> int)
+  }
+/*
+  if (ang <= 45 && ang >= -45){
+    state = 0;
+  }
+  else if (ang <= -45 && ang >= -135){
+    state = 2;
+  }
+  else if (ang >= 45 && ang <= 135){
+    state = 1;
+  }
+  else if (ang >= 135 || ang <= -135){
+    state = 3;
+  }
+*/
+  if (state == 0){
+    if ((ang >= 80 ) && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 1;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+    else if ((ang <= -80 ) && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 2;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+
+    else if ((offset>=10 || offset <= -10) && flagCurveLock){
+
+    u = 45;
+    kp = 0.08;
+    LineFollower.setKp(kp);
+    pid_control_curve(u,offset);
+    
+  }
+
+    else {
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+    }
+    
+  } 
+
+  else if (state == 1){
+    if (ang <=10 && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 0;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+    else if (ang >=170 && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 3;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+    else if ((offset>=10 || offset <= -10) && flagCurveLock){
+
+    u = 45;
+    kp = 0.08;
+    LineFollower.setKp(kp);
+    pid_control_curve(u,offset);
+    
+  }
+
+    else {
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+    }
+
+  }
+
+  else if (state == 2){
+    if (ang <=-170 && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 3;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+    else if (ang >=-10  && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 0;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+
+    else if ((offset>=10 || offset <= -10) && flagCurveLock){
+
+    u = 45;
+    kp = 0.08;
+    LineFollower.setKp(kp);
+    pid_control_curve(u,offset);
+    
+  }
+
+    else {
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+    }
+  }
+
+  else if (state == 3){
+    if ((ang >=-100) && (ang < 0) && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 2;
+      lastLock = millis();
+      flagCurveLock = false;
+
+    }
+    else if ((ang <=100) && (ang > 0) && (offset >=-30 || offset <= 30)){
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+      state = 1;
+      lastLock = millis();
+      flagCurveLock = false;
+    }
+
+    else if ((offset>=10 || offset <= -10) && flagCurveLock){
+
+    u = 45;
+    kp = 0.08;
+    LineFollower.setKp(kp);
+    pid_control_curve(u,offset);
+    
+  }
+
+    else {
+      u = 150;
+      kp = 0.04;
+      LineFollower.setKp(kp);
+      pid_control(u,offset);
+    }
   }
 
   
@@ -77,18 +250,6 @@ void loop() {
   }
   */
 
-  if (offset>=6 || offset <= -6){
-    u = 40;
-    LineFollower.setKp(0.1);
-    pid_control_curve(u,offset);
-    delay(30);
-  }
-
-  else {
-    u = 120;
-    LineFollower.setKp(0.04);
-    pid_control(u,offset);
-  }
   //setMotorBVoltage(u);
   //setMotorAVoltage(0.942*u);
   
@@ -115,9 +276,9 @@ void pid_control_curve(int motor_speed, int sensor_value) {
   //setMotorAVoltage(0);
   //setMotorBVoltage(0);
   int error = sensor_value; // between -512 and 512
-  proportional = p_gain_curve * (error)*motor_speed/20; // replace 10 by max value of steering fct
+  proportional = p_gain_curve * (error)*motor_speed/30; // replace 10 by max value of steering fct
   integral += (i_gain * error)/500;
-  derivative = d_gain * (error - last_error)*motor_speed/25;
+  derivative = d_gain * (error - last_error)*motor_speed/35;
 
   float delta_speed = (proportional + derivative + integral);
   if (motor_speed + delta_speed < speed_limit && motor_speed > 0) {
@@ -126,7 +287,7 @@ void pid_control_curve(int motor_speed, int sensor_value) {
   }
   last_error = error;
   //Serial.println(delta_speed);
-  delay(50);
+  //delay(100);
 }
 
 int getDelaySensorWheel(float tension) {
